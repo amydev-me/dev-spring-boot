88a43d844c2b0f8f303b85471baa3fcdeefb81e3
Amy <ampyaephyonaing@gmail.com>
2023-04-15 22:14:16 +0800

    Custom Package Component Scanning

---
+++ 02-spring-boot-core/02-spring-component-scanning/src/main/java/com/iamcoda/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,16 @@
+package com.iamcoda.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.iamcoda.springcoredemo",
+//							"com.iamcoda.util"})
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
---
+++ 02-spring-boot-core/02-spring-component-scanning/src/main/java/com/iamcoda/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.iamcoda.springcoredemo.rest;
+
+import com.iamcoda.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    // @Autowired annotation tells Spring to inject a dependency
+    // If you only have one constructor then @Autowired on Constructor is optional
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
