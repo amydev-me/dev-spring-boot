d6065b20e9e304134746ffde488e3fe6ef13fec4
Amy <ampyaephyonaing@gmail.com>
2023-04-24 13:45:36 +0800

    Create Multiple Students

--- 03-spring-boot-hibernate-jpa-crud/01-crudemo-student/src/main/java/com/iamcoda/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crudemo-student/src/main/java/com/iamcoda/cruddemo/CruddemoApplication.java
@@ -17,8 +17,29 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipleStudent(studentDAO);
 		};
+	}
+
+	private void createMultipleStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object....");
+		Student tempStudent1 = new Student("Lai", "Mon", "laimon@iamcoda.com");
+		Student tempStudent2 = new Student("Amy", "Pyae", "amy.pyae@iamcoda.com");
+		Student tempStudent3 = new Student("Amy", "Naing", "amy.naing@iamcoda.com");
+
+		//save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id : "  + tempStudent1.getId());
+		System.out.println("Saved student. Generated id : "  + tempStudent2.getId());
+		System.out.println("Saved student. Generated id : "  + tempStudent3.getId());
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
