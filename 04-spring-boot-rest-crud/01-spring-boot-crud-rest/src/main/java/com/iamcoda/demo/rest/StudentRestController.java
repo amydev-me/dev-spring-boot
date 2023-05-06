b6e6dd17ad2df4f5b6808e41c629da2f9097a2b8
Amy <ampyaephyonaing@gmail.com>
2023-05-06 09:48:33 +0800

    Global Exception Handler

--- 04-spring-boot-rest-crud/01-spring-boot-crud-rest/src/main/java/com/iamcoda/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-crud-rest/src/main/java/com/iamcoda/demo/rest/StudentRestController.java
@@ -37,30 +37,4 @@ public class StudentRestController {
         }
         return students.get(studentId);
     }
-
-    // Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleExcelption(StudentNotFoundException exc){
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add exception handler to catch any exception
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
