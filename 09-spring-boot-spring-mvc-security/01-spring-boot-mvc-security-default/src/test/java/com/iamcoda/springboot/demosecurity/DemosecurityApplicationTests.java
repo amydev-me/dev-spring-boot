36c2a6df09455a7cb6aa03d9af7fe04ddc09f468
Amy <ampyaephyonaing@gmail.com>
2023-05-31 23:12:40 +0800

    MVC - Security
    # Login
    # Logout

---
+++ 09-spring-boot-spring-mvc-security/01-spring-boot-mvc-security-default/src/test/java/com/iamcoda/springboot/demosecurity/DemosecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamcoda.springboot.demosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
