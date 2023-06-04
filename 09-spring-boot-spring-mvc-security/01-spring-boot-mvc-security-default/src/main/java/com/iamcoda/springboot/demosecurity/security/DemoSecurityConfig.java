fdc51e0e698c1932921bff4e9d1c7355570d8596
Amy <ampyaephyonaing@gmail.com>
2023-06-04 12:10:04 +0800

    MVC - Security
    # JDBC Login
    # Login with custom user and role table

--- 09-spring-boot-spring-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/iamcoda/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 09-spring-boot-spring-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/iamcoda/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,33 +3,28 @@ package com.iamcoda.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
-    public InMemoryUserDetailsManager userDetailManager(){
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
 
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+
+                "select user_id, pw, active from members where user_id=?"
+        );
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+        return jdbcUserDetailsManager;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configure ->
@@ -52,4 +47,27 @@ public class DemoSecurityConfig {
         return http.build();
 
     }
+
+
+    /**
+     @Bean
+     public InMemoryUserDetailsManager userDetailManager(){
+     UserDetails john = User.builder()
+     .username("john")
+     .password("{noop}test123")
+     .roles("EMPLOYEE")
+     .build();
+     UserDetails mary = User.builder()
+     .username("mary")
+     .password("{noop}test123")
+     .roles("EMPLOYEE", "MANAGER")
+     .build();
+     UserDetails susan = User.builder()
+     .username("susan")
+     .password("{noop}test123")
+     .roles("EMPLOYEE", "MANAGER", "ADMIN")
+     .build();
+     return new InMemoryUserDetailsManager(john, mary, susan);
+     }
+     **/
 }
