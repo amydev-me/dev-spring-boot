7cdef7f37bf09824a67c21eab57fbf69bd4f8e3d
Amy <ampyaephyonaing@gmail.com>
2023-06-08 22:18:03 +0800

    HATEOAS for REST API

--- 11-spring-boot-web-service/01-spring-boot-api/src/main/java/iamcoda/example/springwebservicedemo/rest/UserRestController.java
+++ 11-spring-boot-web-service/01-spring-boot-api/src/main/java/iamcoda/example/springwebservicedemo/rest/UserRestController.java
@@ -4,10 +4,12 @@ import iamcoda.example.springwebservicedemo.dao.UserDaoService;
 import iamcoda.example.springwebservicedemo.entity.User;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
@@ -26,13 +28,16 @@ public class UserRestController {
     }
 
     @GetMapping("/users/{id}")
-    public  User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user =  serviceDao.findById(id);
         if(user == null){
             throw new UserNotFoundException("id" + id);
         }
+        EntityModel<User> entityModel = EntityModel.of(user);
 
-        return user;
+        WebMvcLinkBuilder builder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(builder.withRel("all-users"));
+        return entityModel;
     }
 
     @PostMapping("/users")
