bf5c4c9ce3fe7a319f30f8f473ecdfb252f8340d
Amy <ampyaephyonaing@gmail.com>
2023-06-08 22:50:04 +0800

    RestAPI hide properties using MappingJackson

--- 11-spring-boot-web-service/01-spring-boot-api/src/main/java/iamcoda/example/springwebservicedemo/entity/User.java
+++ 11-spring-boot-web-service/01-spring-boot-api/src/main/java/iamcoda/example/springwebservicedemo/entity/User.java
@@ -1,6 +1,10 @@
 package iamcoda.example.springwebservicedemo.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -8,17 +12,26 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+// hide properties
+//@JsonIgnoreProperties("password")
+
+@JsonFilter("SomeBeanFilter") // name should  same exactly
 public class User {
+
     private Integer id;
 
 
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
 
     @Past(message = "Birth date sßhould be in the past!")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
+//    @JsonIgnore
+    private String password;
     public User(){
 
     }
@@ -27,6 +40,13 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User(Integer id, String name, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.password = password;
     }
 
     public User(String name, LocalDate birthDate) {
@@ -57,6 +77,14 @@ public class User {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
--- 11-spring-boot-web-service/01-spring-boot-api/src/main/java/iamcoda/example/springwebservicedemo/rest/UserRestController.java
+++ 11-spring-boot-web-service/01-spring-boot-api/src/main/java/iamcoda/example/springwebservicedemo/rest/UserRestController.java
@@ -1,5 +1,8 @@
 package iamcoda.example.springwebservicedemo.rest;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import iamcoda.example.springwebservicedemo.dao.UserDaoService;
 import iamcoda.example.springwebservicedemo.entity.User;
 import jakarta.validation.Valid;
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -20,6 +24,49 @@ public class UserRestController {
     @Autowired
     public UserRestController(UserDaoService serviceDao){
         this.serviceDao = serviceDao;
+    }
+
+    @GetMapping("/filtering/{id}")
+    public MappingJacksonValue filtering(@PathVariable int id){
+        User user =  serviceDao.findById(id);
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(user);
+        SimpleBeanPropertyFilter filter =
+                SimpleBeanPropertyFilter.filterOutAllExcept("user_name", "birth_date");
+
+        FilterProvider filters =
+                new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+    }
+
+    @GetMapping("/filtering-list")
+    public MappingJacksonValue filteringList(){
+        List<User> users = serviceDao.findAll();
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(users);
+
+        SimpleBeanPropertyFilter filter =
+                SimpleBeanPropertyFilter.filterOutAllExcept("user_name", "birth_date");
+
+        FilterProvider filters =
+                new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
+    }
+
+
+    @GetMapping("/hide-field")
+    public EntityModel<User> retrieveUserWithoutPassword(@PathVariable int id){
+        User user =  serviceDao.findById(id);
+        if(user == null){
+            throw new UserNotFoundException("id" + id);
+        }
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder builder = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(builder.withRel("all-users"));
+        return entityModel;
     }
 
     @GetMapping("/users")
